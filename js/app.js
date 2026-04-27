4a3c6e6c94c04f5fbd2ffc02a497d6fc94051d9e
VictorArguelho <varguelho69@gmail.com>
2026-04-27 02:02:49 -0300

    feat: add format currency and percent system

---
+++ js/app.js
@@ -0,0 +1,78 @@
+const elements = Object.freeze({
+  startAmount: document.getElementById('start-amount'),
+  contribution: document.getElementById('contribution'),
+  rate: document.getElementById('rate'),
+});
+
+setupCurrencyInput(elements.startAmount);
+setupCurrencyInput(elements.contribution);
+setupPercentInput(elements.rate);
+
+function setupCurrencyInput(input) {
+  setupDecimalInput(input, 'R$ ', 'prefix');
+}
+
+function setupPercentInput(input) {
+  setupDecimalInput(input, '%', 'suffix');
+}
+
+function setupDecimalInput(input, symbol, position) {
+  if (position === 'suffix') {
+    input.addEventListener('beforeinput', (event) =>
+      handleSuffixDeleteFix(event, symbol.length),
+    );
+  }
+  input.addEventListener('input', (event) =>
+    handleDecimalInput(event, symbol, position),
+  );
+}
+
+function handleDecimalInput(event, symbol, position) {
+  const target = event.target;
+  const digits = getDigits(target.value);
+
+  if (digits === '') {
+    target.value = '';
+    return;
+  }
+
+  if (position === 'prefix') {
+    target.value = `${symbol}${formatDecimalBR(digits)}`;
+  }
+
+  if (position === 'suffix') {
+    target.value = `${formatDecimalBR(digits)}${symbol}`;
+  }
+}
+
+function handleSuffixDeleteFix(event, suffixLength) {
+  if (event.inputType === 'deleteContentBackward') {
+    const target = event.target;
+
+    const start = target.selectionStart;
+    const end = target.selectionEnd;
+
+    if (start !== end) {
+      return;
+    }
+
+    const valueLimit = target.value.length - suffixLength;
+
+    if (start > valueLimit) {
+      target.value = target.value.slice(0, valueLimit);
+    }
+  }
+}
+
+function formatDecimalBR(digits) {
+  const amount = Number(digits) / 100;
+
+  return amount.toLocaleString('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
+
+function getDigits(text) {
+  return text.replace(/\D/g, '');
+}
